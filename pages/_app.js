3b38e8169a5cee87a63ab62723fcf97472ecd100
JoyAnneW <joyawilliams@gmail.com>
2022-11-14 16:30:09 +0100

    setup of Chakra UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
-import "../styles/globals.css";
 import { Provider, createClient } from "urql";
+import { ChakraProvider } from "@chakra-ui/react";
 
 // this is the address to the backend. Provider below gives access to the backend to our app
 const client = createClient({ url: process.env.NEXT_PUBLIC_BACKEND_API });
 function MyApp({ Component, pageProps }) {
 	return (
 		<Provider value={client}>
-			<Component {...pageProps} />
+			<ChakraProvider>
+				<Component {...pageProps} />
+			</ChakraProvider>
 		</Provider>
 	);
 }
