839281ce6592b766aa651ee5990f4b45e67a7eeb
JoyAnneW <joyawilliams@gmail.com>
2022-11-14 20:52:15 +0100

    initial stylingwof product cards and grid display

--- components/Product.js
+++ components/Product.js
@@ -1,14 +1,40 @@
+import {
+	Button,
+	ButtonGroup,
+	Card,
+	CardBody,
+	CardFooter,
+	Divider,
+	Heading,
+	Image,
+	Stack,
+	Text,
+} from "@chakra-ui/react";
+
 export default function Product({ product }) {
 	const { image, name, price, description } = product.attributes;
 	const mdImg = image.data.attributes.formats.medium.url;
 	return (
-		<div>
-			<div>
-				<img src={mdImg} className="img-fluid rounded-top" />
-			</div>
-			<h2>{name}</h2>
-			<h3>{price}</h3>
-			<h3>{description}</h3>
-		</div>
+		<Card>
+			<CardBody>
+				<Image src={mdImg} borderRadius="lg" />
+				<Stack mt="6">
+					<Heading size="md">{name}</Heading>
+					<Text>{description}</Text>
+					<Text fontSize="2xl">€{price}</Text>
+				</Stack>
+			</CardBody>
+			<Divider />
+			<CardFooter>
+				<ButtonGroup spacing="2">
+					<Button variant="solid" colorScheme="orange">
+						Buy now
+					</Button>
+					<Button variant="ghost" colorScheme="orange">
+						Add to cart
+					</Button>
+				</ButtonGroup>
+			</CardFooter>
+		</Card>
 	);
 }
