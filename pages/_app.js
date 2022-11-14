6abb8a5490c2b290e12f1a842e8fda8e5d332363
JoyAnneW <joyawilliams@gmail.com>
2022-11-14 15:04:08 +0100

    installed graphql and urql. hooked up frontend to backend. product query is saved in lib folder. created product page with dynamic route segment.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Provider, createClient } from "urql";
 
+// this is the address to the backend. Provider below gives access to the backend to our app
+const client = createClient({ url: "http://localhost:1337/graphql" });
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<Provider value={client}>
+			<Component {...pageProps} />
+		</Provider>
+	);
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/product/[id].js
@@ -0,0 +1,7 @@
+export default function about() {
+	return (
+		<div>
+			<h1>About Page for product</h1>
+		</div>
+	);
+}
