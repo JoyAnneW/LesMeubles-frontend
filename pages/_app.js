e4c1269a13ae10cd67db5a0cbc70b53c4a0b4c99
JoyAnneW <joyawilliams@gmail.com>
2022-11-14 16:23:40 +0100

    added backend api as env variable

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import { Provider, createClient } from "urql";
 
 // this is the address to the backend. Provider below gives access to the backend to our app
-const client = createClient({ url: "http://localhost:1337/graphql" });
+const client = createClient({ url: process.env.NEXT_PUBLIC_BACKEND_API });
 function MyApp({ Component, pageProps }) {
 	return (
 		<Provider value={client}>
