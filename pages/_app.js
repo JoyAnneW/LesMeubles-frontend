573359b3cddf58aca23730f9a9b3d68712ee7073
JoyAnneW <joyawilliams@gmail.com>
2022-11-23 15:58:30 +0100

    initial config of auth0

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,21 @@ import { Provider, createClient } from "urql";
 import { ChakraProvider } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 import { ShopContextProvider } from "../lib/context";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
 // this is the address to the backend. Provider below gives access to the backend to our app
 const client = createClient({ url: process.env.NEXT_PUBLIC_BACKEND_API });
 function MyApp({ Component, pageProps }) {
 	return (
 		<Provider value={client}>
-			<ChakraProvider>
-				<ShopContextProvider>
-					<Navbar />
-					<Component {...pageProps} />
-				</ShopContextProvider>
-			</ChakraProvider>
+			<UserProvider>
+				<ChakraProvider>
+					<ShopContextProvider>
+						<Navbar />
+						<Component {...pageProps} />
+					</ShopContextProvider>
+				</ChakraProvider>
+			</UserProvider>
 		</Provider>
 	);
 }
